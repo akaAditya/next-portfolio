f98731735c37dfbb88f1ce7a690fe371811c42eb
akaAditya <mymail.adityapatel@gmail.com>
2023-10-11 03:49:19 +0530

    third commit

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -1,8 +1,24 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from "react";
 
-import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
-import { Section, SectionDivider, SectionText, SectionTitle, SubSectionText } from '../../styles/GlobalComponents';
-import { TimeLineData } from '../../constants/constants';
+import {
+  CarouselButton,
+  CarouselButtonDot,
+  CarouselButtons,
+  CarouselContainer,
+  CarouselItem,
+  CarouselItemImg,
+  CarouselItemText,
+  CarouselItemTitle,
+  CarouselMobileScrollNode,
+} from "./TimeLineStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+  SubSectionText,
+} from "../../styles/GlobalComponents";
+import { TimeLineData } from "../../constants/constants";
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
@@ -11,70 +27,77 @@ const Timeline = () => {
   const carouselRef = useRef();
 
   const scroll = (node, left) => {
-    return node.scrollTo({ left, behavior: 'smooth' });
-  }
+    return node.scrollTo({ left, behavior: "smooth" });
+  };
 
   const handleClick = (e, i) => {
     e.preventDefault();
 
     if (carouselRef.current) {
-      const scrollLeft = Math.floor(carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length));
-      
+      const scrollLeft = Math.floor(
+        carouselRef.current.scrollWidth * 0.7 * (i / TimeLineData.length)
+      );
+
       scroll(carouselRef.current, scrollLeft);
     }
-  }
+  };
 
   const handleScroll = () => {
     if (carouselRef.current) {
-      const index = Math.round((carouselRef.current.scrollLeft / (carouselRef.current.scrollWidth * 0.7)) * TimeLineData.length);
+      const index = Math.round(
+        (carouselRef.current.scrollLeft /
+          (carouselRef.current.scrollWidth * 0.7)) *
+          TimeLineData.length
+      );
 
       setActiveItem(index);
     }
-  }
+  };
 
   // // snap back to beginning of scroll when window is resized
   // // avoids a bug where content is covered up if coming from smaller screen
   useEffect(() => {
     const handleResize = () => {
       scroll(carouselRef.current, 0);
-    }
+    };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
   }, []);
 
   return (
     <Section>
       <SectionTitle id="about">About Me</SectionTitle>
       <SectionText>
-        Web Development is one of the best stream for a great a career in this Digital World. I define myself
-        by the work I do. Skills can be taught, personality is inherent. 
-        <br /> 
-        <br /> 
-        I prefer to keep learning, continue challenging myself to accelerate thinking abilities.
-        I am passionate, expressive, multi-talented
-        spirit with a natural ability to entertain and inspire. 
-        <br /> 
-        <br /> 
-        I am a fast learner,
-        able to pick up new skills and juggle different projects and roles
-        with relative ease. I like to develop expertise in a number of areas
-        over the course of my life and career.
+        Web Development is one of the best stream for a great a career in this
+        Digital World. I define myself by the work I do. Skills can be taught,
+        personality is inherent.
+        <br />
+        <br />
+        I prefer to keep learning, continue challenging myself to accelerate
+        thinking abilities. I am passionate, expressive, multi-talented spirit
+        with a natural ability to entertain and inspire.
+        <br />
+        <br />I am a fast learner, able to pick up new skills and juggle
+        different projects and roles with relative ease. I like to develop
+        expertise in a number of areas over the course of my life and career.
       </SectionText>
       <SectionText>
         {/* <div className="about-me">
           I have worked in <b ><a href='https://deepcognition.ai' style={{color: 'white'}} target='_blank'>DeepCognition.ai</a></b> company as a Data Annotation Specialists. On other hand, I have also worked on Front-End Development of <b><a href="https://deepcognition.ai/introduction/" target='_blank' style={{color: 'white'}}>paperentry.ai</a></b> software. 
         </div> */}
-        
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
-        {TimeLineData.map((item, index) =>(
-          <CarouselMobileScrollNode key={index} final={index=== TOTAL_CAROUSEL_COUNT - 1}>
-            <CarouselItem
-              index={index}
-              id={`carousel__item-${index}`}
-              active= {activeItem}
-              onClick={(e)=> handleClick(e, index)}
+          {TimeLineData.map((item, index) => (
+            <CarouselMobileScrollNode
+              key={index}
+              final={index === TOTAL_CAROUSEL_COUNT - 1}
+            >
+              <CarouselItem
+                index={index}
+                id={`carousel__item-${index}`}
+                active={activeItem}
+                onClick={(e) => handleClick(e, index)}
               >
                 <CarouselItemTitle>
                   {item.year}
@@ -83,7 +106,8 @@ const Timeline = () => {
                     height="6"
                     viewBox="0 0 208 6"
                     fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
                     <path
                       fill-rule="evenodd"
                       clip-rule="evenodd"
@@ -98,7 +122,8 @@ const Timeline = () => {
                         y1="0.5"
                         x2="208"
                         y2="0.500295"
-                        gradientUnits="userSpaceOnUse">
+                        gradientUnits="userSpaceOnUse"
+                      >
                         <stop stop-color="white" />
                         <stop
                           offset="0.79478"
@@ -110,27 +135,26 @@ const Timeline = () => {
                   </CarouselItemImg>
                 </CarouselItemTitle>
                 <CarouselItemText>{item.text}</CarouselItemText>
-              </CarouselItem> 
-          </CarouselMobileScrollNode>
-        ))}
+              </CarouselItem>
+            </CarouselMobileScrollNode>
+          ))}
         </>
       </CarouselContainer>
       <CarouselButtons>
-        {TimeLineData.map((item, index)=>(
+        {TimeLineData.map((item, index) => (
           <CarouselButton
-          key={index}
-          index={index}
-          active={activeItem}
-          onClick={(e) => handleClick(e, index)}
-          type= "button"
+            key={index}
+            index={index}
+            active={activeItem}
+            onClick={(e) => handleClick(e, index)}
+            type="button"
           >
-            <CarouselButtonDot active={activeItem}/>
-      </CarouselButton>
-
+            <CarouselButtonDot active={activeItem} />
+          </CarouselButton>
         ))}
       </CarouselButtons>
-      
-      <SectionDivider /> 
+
+      <SectionDivider />
     </Section>
   );
 };
