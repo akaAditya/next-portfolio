fdca21c2841cb25d05d670edda85a053741d98a4
akaAditya <mymail.adityapatel@gmail.com>
2024-07-02 11:32:46 +0530

    correctioin commit

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,21 @@
 // import resume from "../../public/resume"
-export const resume = '/resume/AdityaKumarPatel_SDE.pdf'
+export const resume = "/resume/AdityaKumarPatel_SDE.pdf";
 export const projects = [
+  {
+    title: "FlixLuxe Online Movie WebApp",
+    description:
+      "The project aimed to offer an intuitive and immersive experience for discovering, selecting, and watching content.",
+    image: "/images/movieposter.jpg",
+    tags: ["React.js", "CSS", "HTML5", "Firebase", "Stripe"],
+    // source: "https://github.com/akaAditya/Netflix-Clone-App",
+    visit: "https://astonishing-bavarois-832e20.netlify.app/",
+    id: 3,
+  },
   {
     title: "Book Store",
     description: "Explore the Most Readable Books Collection",
     image: "/images/bookstore.jpg",
-    tags: ["React.js",'Express.js','Node.js', 'Mongoose'],
+    tags: ["React.js", "Express.js", "Node.js", "Mongoose"],
     // source: "https://github.com/akaAditya/BookStore",
     visit: "https://frolicking-kitten-2136b7.netlify.app/",
     id: 0,
@@ -24,39 +34,28 @@ export const projects = [
     description:
       "Most of the time you forget to analyse your monthly expenses and got frustrated where all your money gone. So this project organized to solve your expenses problems.",
     image: "/images/expenseTracker.jpg",
-    tags: ["React.js","Redux-Toolkit", "CSS3", "Firebase"],
+    tags: ["React.js", "Redux-Toolkit", "CSS3", "Firebase"],
     // source: "https://github.com/akaAditya/react-user-auth-2",
     visit: "https://resonant-kashata-618244.netlify.app/",
     id: 2,
-  },
-  {
-    title: "FlixLuxe Online Movie WebApp",
-    description:
-      "The project aimed to offer an intuitive and immersive experience for discovering, selecting, and watching content.",
-    image: "/images/movieposter.jpg",
-    tags: ["React.js", "CSS", "HTML5", "Firebase", "Stripe"],
-    // source: "https://github.com/akaAditya/Netflix-Clone-App",
-    visit: "https://astonishing-bavarois-832e20.netlify.app/",
-    id: 3,
   },
 ];
 
 export const TimeLineData = [
   {
     year: 2018,
-    text: 'Completed B.E. in CSE' 
+    text: "Completed B.E. in CSE",
   },
   {
     year: 2019,
-    text: "I gave Maths & Science coaching to SSC students",
+    text: "Maths & Science coaching to SSC students",
   },
   {
     year: 2021,
-    text: "Associate Software Engineer in Deep Cognition India Pvt. Ltd. Bengaluru",
+    text: "Data Annotation Specialists in Deep Cognition India Pvt. Ltd. Bengaluru",
   },
   {
     year: 2023,
     text: "MERN Stack Certification Course from Sharpener.tech",
   },
 ];
-
